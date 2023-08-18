4ae3e875539d2b1c2802696b451c68918aefb167
VENKAT S P <109540044+venkatsp17@users.noreply.github.com>
2023-08-18 15:12:56 +0530

    routes configured⚡️

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
-import Main from './pages/Main';
+import "./App.css";
+import Main from "./pages/Main";
+import BrowserRouter from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Main/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

3345f10cbcbfdcdb412761f45abde795b0ce4226
VENKAT S P <109540044+venkatsp17@users.noreply.github.com>
2023-08-18 23:42:42 +0530

    Login, Create Account Pages UI🚀

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import Main from "./pages/Main";
-import BrowserRouter from "react-router-dom";
+import Login from "./pages/Login";
+import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
+import CreateAccount from "./pages/CreateAccount";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/create' element={<CreateAccount />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../css/Main.css';
 
 const Main = ()=>{
     <div></div>
